0831bc3f7d394975a8fe0b8a0ef63c317d9b5a57
Mengyu <785144001@qq.com>
2017-06-16 09:43:34 +0800

    linewidth

--- project/QtProject/map/creategridplane.cpp
+++ project/QtProject/map/creategridplane.cpp
@@ -1,11 +1,14 @@
 #include <osg/Geode>
 #include <osg/Geometry>
+#include <osg/LineWidth>
 #include <osg/MatrixTransform>
+#include <osg/PolygonOffset>
 
 osg::ref_ptr<osg::Node> createGridPlane()
 {
     // 创建一个用于保存几何信息的对象
-    osg::ref_ptr<osg::Geometry> GeometryPlane = new osg::Geometry;
+    osg::ref_ptr<osg::Geometry> GeometryPlane =
+            new osg::Geometry;
     // 创建四个顶点的数组
     osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
     GeometryPlane->setVertexArray(v.get());
@@ -16,7 +19,8 @@ osg::ref_ptr<osg::Node> createGridPlane()
     // 创建四种颜色的数组
     osg::ref_ptr<osg::Vec4Array> c = new osg::Vec4Array;
     GeometryPlane->setColorArray(c.get());
-    GeometryPlane->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    GeometryPlane->setColorBinding(
+            osg::Geometry::BIND_PER_VERTEX);
     c->push_back(osg::Vec4(0.f, 0.f, 1.f, 1.f));
     c->push_back(osg::Vec4(0.f, 0.f, 1.f, 1.f));
     c->push_back(osg::Vec4(0.f, 0.f, 1.f, 1.f));
@@ -24,13 +28,19 @@ osg::ref_ptr<osg::Node> createGridPlane()
     // 为唯一的法线创建一个数组
     osg::ref_ptr<osg::Vec3Array> n = new osg::Vec3Array;
     GeometryPlane->setNormalArray(n.get());
-    GeometryPlane->setNormalBinding(osg::Geometry::BIND_OVERALL);
+    GeometryPlane->setNormalBinding(
+            osg::Geometry::BIND_OVERALL);
     n->push_back(osg::Vec3(0.f, -1.f, 0.f));
     // 由保存的数据绘制四个顶点的多边形
     GeometryPlane->addPrimitiveSet(new osg::DrawArrays(
             osg::PrimitiveSet::QUADS, 0, 4));
+    osg::ref_ptr<osg::StateSet> SetPlane =
+            new osg::StateSet();
+    SetPlane->setMode(
+            GL_LIGHTING, osg::StateAttribute::OFF);
     osg::ref_ptr<osg::Geode> GeodePlane = new osg::Geode;
     GeodePlane->addDrawable(GeometryPlane.get());
+    GeodePlane->setStateSet(SetPlane);
 
     // draw the gird
     osg::ref_ptr<osg::Geode> GeodeGrid = new osg::Geode();
@@ -42,10 +52,10 @@ osg::ref_ptr<osg::Node> createGridPlane()
     int num = 80;
     for (i = -80; i <= num; i += 20)
     {
-        gridv->push_back(osg::Vec3(i, 105.0f, 0.0f));
-        gridv->push_back(osg::Vec3(i, -105.0f, 0.0f));
-        gridv->push_back(osg::Vec3(105.0f, i, 0.0f));
-        gridv->push_back(osg::Vec3(-105.0f, i, 0.0f));
+        gridv->push_back(osg::Vec3(i, 102.0f, 0.0f));
+        gridv->push_back(osg::Vec3(i, -102.0f, 0.0f));
+        gridv->push_back(osg::Vec3(102.0f, i, 0.0f));
+        gridv->push_back(osg::Vec3(-102.0f, i, 0.0f));
     }
     GeometryGrid->setVertexArray(gridv);
     // vertify the color
@@ -66,10 +76,19 @@ osg::ref_ptr<osg::Node> createGridPlane()
     GeometryGrid->addPrimitiveSet(new osg::DrawArrays(
             osg::PrimitiveSet::LINES, 0, gridv->size()));
     GeodeGrid->addDrawable(GeometryGrid);
-    osg::ref_ptr<osg::StateSet> set = new osg::StateSet();
-    set->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
-    set->setMode(GL_LINE_SMOOTH, osg::StateAttribute::ON);
-    GeodeGrid->setStateSet(set);
+    osg::ref_ptr<osg::StateSet> SetGrid =
+            new osg::StateSet();
+    SetGrid->setMode(
+            GL_LIGHTING, osg::StateAttribute::PROTECTED);
+    SetGrid->setMode(
+            GL_LINE_SMOOTH, osg::StateAttribute::ON);
+    SetGrid->setAttributeAndModes(
+            new osg::PolygonOffset(1.0f, 1.0f),
+            osg::StateAttribute::ON);
+    osg::ref_ptr<osg::LineWidth> lw =
+            new osg::LineWidth(3.f);
+    SetGrid->setAttribute(lw);
+    GeodeGrid->setStateSet(SetGrid);
 
     osg::ref_ptr<osg::Group> GridPlane = new osg::Group;
     // 向 Geode 类添加几何体(Drawable)并返回 Geode
--- project/QtProject/map/createtext.cpp
+++ project/QtProject/map/createtext.cpp
@@ -12,9 +12,9 @@ osg::ref_ptr<osg::Node> createText()
     osg::ref_ptr<osgText::Text> text = new osgText::Text();
     text->setFont(font.get());
     text->setColor(white);
-    text->setCharacterSize(10.f);
-    text->setPosition(osg::Vec3(0.0f, -110.0f, 0.0f));
-    text->setText("E 120");
+    text->setCharacterSize(3.f);
+    text->setPosition(osg::Vec3(-4.f, -110.0f, 0.0f));
+    text->setText("120E");
     text->setAxisAlignment(osgText::Text::XY_PLANE);
     GeodeText->addDrawable(text.get());
     return GeodeText.get();
