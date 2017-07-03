f21012faac50907d7097c712b267f2e5c1ace57c
Mengyu <785144001@qq.com>
2017-07-03 14:36:56 +0800

    add UAV's number

--- project/QtProject/EventHandleUAV/uav.cpp
+++ project/QtProject/EventHandleUAV/uav.cpp
@@ -3,12 +3,12 @@
 UAV::UAV() {}
 
 osg::ref_ptr<osg::Node> UAV::CreateGlider(
-/*        float x1, float y1, float z1, float x2, float y2,
+        /*        float x1, float y1, float z1, float x2, float y2,
         float z2,*/ int a)
 {
     osg::ref_ptr<osg::Node> glider = osgDB::readNodeFile(
             "/home/mengyu/material/"
-            "gliderZoomIn100.osg");
+            "gliderZoomIn10.osg");
     osg::ref_ptr<osg::Image> skin = new osg::Image;
     switch (a)
     {
@@ -39,17 +39,18 @@ osg::ref_ptr<osg::Node> UAV::CreateGlider(
             break;
     }
 
-//    GliderTrans = new osg::PositionAttitudeTransform();
-//    GLiderPath = new osg::AnimationPath;
-//    GLiderPath->insert(
-//            0, osg::AnimationPath::ControlPoint(
-//                       osg::Vec3(x1, y1, z1)));
-//    GLiderPath->insert(
-//            20, osg::AnimationPath::ControlPoint(
-//                        osg::Vec3(x2, y2, z2)));
-//    GliderTrans->setUpdateCallback(
-//            new osg::AnimationPathCallback(
-//                    GLiderPath, 0.0, 1.0));
+    //    GliderTrans = new
+    //    osg::PositionAttitudeTransform();
+    //    GLiderPath = new osg::AnimationPath;
+    //    GLiderPath->insert(
+    //            0, osg::AnimationPath::ControlPoint(
+    //                       osg::Vec3(x1, y1, z1)));
+    //    GLiderPath->insert(
+    //            20, osg::AnimationPath::ControlPoint(
+    //                        osg::Vec3(x2, y2, z2)));
+    //    GliderTrans->setUpdateCallback(
+    //            new osg::AnimationPathCallback(
+    //                    GLiderPath, 0.0, 1.0));
     osg::ref_ptr<osg::Texture2D> texture =
             new osg::Texture2D;
     texture->setImage(skin.get());
@@ -65,97 +66,126 @@ osg::ref_ptr<osg::Node> UAV::CreateGlider(
             1, texgen.get(), osg::StateAttribute::ON);
     state->setTextureAttributeAndModes(1, texenv.get());
     glider->setStateSet(state.get());
-//    GliderTrans->addChild(glider);
+    // text
+    osg::ref_ptr<osg::Geode> textGeode = new osg::Geode;
+    osg::ref_ptr<osgText::Font> font =
+            osgText::readFontFile(
+                    "/home/mengyu/material/data/fonts/"
+                    "arial.ttf");
+    osg::ref_ptr<osgText::Text> text = new osgText::Text;
+    text->setFont(font.get());
+    text->setText("1");
+    text->setPosition(osg::Vec3(0.f, 0.f, 10.f));
+    text->setAxisAlignment(osgText::Text::SCREEN);
+    text->setAlignment(osgText::Text::CENTER_BOTTOM);
+    text->setCharacterSize(4.f);
+    textGeode->addDrawable(text.get());
+    //    GliderTrans->addChild(glider);
 
     //     Draw line
     // Primite
-//    osg::ref_ptr<osg::Geode> GeodeLine = new osg::Geode;
-//    osg::ref_ptr<osg::Geometry> GeometryLine =
-//            new osg::Geometry;
-//    osg::ref_ptr<osg::Vec3Array> vLine = new osg::Vec3Array;
-//    vLine->push_back(osg::Vec3(0, 0, 0));
-//    vLine->push_back(osg::Vec3(0, 0, z1));
-//    GeometryLine->setVertexArray(vLine.get());
-//    osg::ref_ptr<osg::Vec4Array> cLine = new osg::Vec4Array;
-//    cLine->push_back(osg::Vec4(0.f, 1.0f, 0.0f, 1.0f));
-//    GeometryLine->setColorArray(cLine.get());
-//    GeometryLine->setColorBinding(
-//            osg::Geometry::BIND_OVERALL);
-//    GeometryLine->addPrimitiveSet(new osg::DrawArrays(
-//            osg::PrimitiveSet::LINES, 0, 2));
-//    GeodeLine->addDrawable(GeometryLine);
+    //    osg::ref_ptr<osg::Geode> GeodeLine = new
+    //    osg::Geode;
+    //    osg::ref_ptr<osg::Geometry> GeometryLine =
+    //            new osg::Geometry;
+    //    osg::ref_ptr<osg::Vec3Array> vLine = new
+    //    osg::Vec3Array;
+    //    vLine->push_back(osg::Vec3(0, 0, 0));
+    //    vLine->push_back(osg::Vec3(0, 0, z1));
+    //    GeometryLine->setVertexArray(vLine.get());
+    //    osg::ref_ptr<osg::Vec4Array> cLine = new
+    //    osg::Vec4Array;
+    //    cLine->push_back(osg::Vec4(0.f, 1.0f, 0.0f,
+    //    1.0f));
+    //    GeometryLine->setColorArray(cLine.get());
+    //    GeometryLine->setColorBinding(
+    //            osg::Geometry::BIND_OVERALL);
+    //    GeometryLine->addPrimitiveSet(new osg::DrawArrays(
+    //            osg::PrimitiveSet::LINES, 0, 2));
+    //    GeodeLine->addDrawable(GeometryLine);
 
-//    // position and scale
-//    osg::ref_ptr<osg::MatrixTransform> mt =
-//            new osg::MatrixTransform();
-//    mt->setName("AnimatedNode");
-//    mt->setDataVariance(osg::Object::DYNAMIC);
-//    mt->addChild(GeodeLine);
-//    osg::ref_ptr<osgAnimation::UpdateMatrixTransform> umt =
-//            new osgAnimation::UpdateMatrixTransform(
-//                    "AnimatedCallback");
-//    umt->getStackedTransforms().push_back(
-//            new osgAnimation::StackedScaleElement("scale"));
-//    umt->getStackedTransforms().push_back(
-//            new osgAnimation::StackedTranslateElement(
-//                    "position"));
-//    mt->setUpdateCallback(umt);
-//    // channelScale
-//    osg::ref_ptr<osgAnimation::Vec3LinearChannel>
-//            channelScale =
-//                    new osgAnimation::Vec3LinearChannel();
-//    channelScale->setName("scale");
-//    channelScale->setTargetName("AnimatedCallback");
-//    channelScale->getOrCreateSampler()
-//            ->getOrCreateKeyframeContainer()
-//            ->push_back(osgAnimation::Vec3Keyframe(
-//                    0, osg::Vec3(1.f, 1.f, 1.f)));
-//    channelScale->getOrCreateSampler()
-//            ->getOrCreateKeyframeContainer()
-//            ->push_back(osgAnimation::Vec3Keyframe(
-//                    20, osg::Vec3(1.f, 1.f, z2 / z1)));
-//    osg::ref_ptr<osgAnimation::Animation> anim1 =
-//            new osgAnimation::Animation;
-//    anim1->addChannel(channelScale);
-//    anim1->setPlayMode(osgAnimation::Animation::LOOP);
-//    // channelPosition
-//    osg::ref_ptr<osgAnimation::Vec3LinearChannel>
-//            channelPosition =
-//                    new osgAnimation::Vec3LinearChannel;
-//    channelPosition->setName("position");
-//    channelPosition->setTargetName("AnimatedCallback");
-//    channelPosition->getOrCreateSampler()
-//            ->getOrCreateKeyframeContainer()
-//            ->push_back(osgAnimation::Vec3Keyframe(
-//                    0, osg::Vec3(x1, y1, 0.f)));
-//    channelPosition->getOrCreateSampler()
-//            ->getOrCreateKeyframeContainer()
-//            ->push_back(osgAnimation::Vec3Keyframe(
-//                    20, osg::Vec3(x2, y2, 0.f)));
-//    osg::ref_ptr<osgAnimation::Animation> anim2 =
-//            new osgAnimation::Animation;
-//    anim2->addChannel(channelPosition);
-//    anim2->setPlayMode(osgAnimation::Animation::LOOP);
+    //    // position and scale
+    //    osg::ref_ptr<osg::MatrixTransform> mt =
+    //            new osg::MatrixTransform();
+    //    mt->setName("AnimatedNode");
+    //    mt->setDataVariance(osg::Object::DYNAMIC);
+    //    mt->addChild(GeodeLine);
+    //    osg::ref_ptr<osgAnimation::UpdateMatrixTransform>
+    //    umt =
+    //            new osgAnimation::UpdateMatrixTransform(
+    //                    "AnimatedCallback");
+    //    umt->getStackedTransforms().push_back(
+    //            new
+    //            osgAnimation::StackedScaleElement("scale"));
+    //    umt->getStackedTransforms().push_back(
+    //            new osgAnimation::StackedTranslateElement(
+    //                    "position"));
+    //    mt->setUpdateCallback(umt);
+    //    // channelScale
+    //    osg::ref_ptr<osgAnimation::Vec3LinearChannel>
+    //            channelScale =
+    //                    new
+    //                    osgAnimation::Vec3LinearChannel();
+    //    channelScale->setName("scale");
+    //    channelScale->setTargetName("AnimatedCallback");
+    //    channelScale->getOrCreateSampler()
+    //            ->getOrCreateKeyframeContainer()
+    //            ->push_back(osgAnimation::Vec3Keyframe(
+    //                    0, osg::Vec3(1.f, 1.f, 1.f)));
+    //    channelScale->getOrCreateSampler()
+    //            ->getOrCreateKeyframeContainer()
+    //            ->push_back(osgAnimation::Vec3Keyframe(
+    //                    20, osg::Vec3(1.f, 1.f, z2 /
+    //                    z1)));
+    //    osg::ref_ptr<osgAnimation::Animation> anim1 =
+    //            new osgAnimation::Animation;
+    //    anim1->addChannel(channelScale);
+    //    anim1->setPlayMode(osgAnimation::Animation::LOOP);
+    //    // channelPosition
+    //    osg::ref_ptr<osgAnimation::Vec3LinearChannel>
+    //            channelPosition =
+    //                    new
+    //                    osgAnimation::Vec3LinearChannel;
+    //    channelPosition->setName("position");
+    //    channelPosition->setTargetName("AnimatedCallback");
+    //    channelPosition->getOrCreateSampler()
+    //            ->getOrCreateKeyframeContainer()
+    //            ->push_back(osgAnimation::Vec3Keyframe(
+    //                    0, osg::Vec3(x1, y1, 0.f)));
+    //    channelPosition->getOrCreateSampler()
+    //            ->getOrCreateKeyframeContainer()
+    //            ->push_back(osgAnimation::Vec3Keyframe(
+    //                    20, osg::Vec3(x2, y2, 0.f)));
+    //    osg::ref_ptr<osgAnimation::Animation> anim2 =
+    //            new osgAnimation::Animation;
+    //    anim2->addChannel(channelPosition);
+    //    anim2->setPlayMode(osgAnimation::Animation::LOOP);
 
-//    osg::ref_ptr<osg::Group> LineGroup = new osg::Group;
-//    osg::ref_ptr<osgAnimation::BasicAnimationManager> bam =
-//            new osgAnimation::BasicAnimationManager;
+    //    osg::ref_ptr<osg::Group> LineGroup = new
+    //    osg::Group;
+    //    osg::ref_ptr<osgAnimation::BasicAnimationManager>
+    //    bam =
+    //            new osgAnimation::BasicAnimationManager;
 
-//    osg::ref_ptr<osg::Group> root = new osg::Group;
+    //    osg::ref_ptr<osg::Group> root = new osg::Group;
 
-//    root->addChild(mt);
-//    LineGroup->addChild(root);
-//    LineGroup->setUpdateCallback(bam);
-//    // register and play the animations
-//    bam->registerAnimation(anim1);
-//    bam->registerAnimation(anim2);
-//    bam->playAnimation(anim1);
-//    bam->playAnimation(anim2);
+    //    root->addChild(mt);
+    //    LineGroup->addChild(root);
+    //    LineGroup->setUpdateCallback(bam);
+    //    // register and play the animations
+    //    bam->registerAnimation(anim1);
+    //    bam->registerAnimation(anim2);
+    //    bam->playAnimation(anim1);
+    //    bam->playAnimation(anim2);
 
     // Group
-//    osg::ref_ptr<osg::Group> GliderLine = new osg::Group;
-//    GliderLine->addChild(GliderTrans);
-//    GliderLine->addChild(LineGroup);
-//    return GliderLine;
-    return glider;
+    //    osg::ref_ptr<osg::Group> GliderLine = new
+    //    osg::Group;
+    //    GliderLine->addChild(GliderTrans);
+    //    GliderLine->addChild(LineGroup);
+    //    return GliderLine;
+    osg::ref_ptr<osg::Group> grp = new osg::Group;
+    grp->addChild(glider);
+    grp->addChild(textGeode.get());
+    return grp;
 }
--- project/QtProject/EventHandleUAV/uav.h
+++ project/QtProject/EventHandleUAV/uav.h
@@ -1,25 +1,25 @@
 #ifndef UAV_H
 #define UAV_H
 
-
 #include <osg/AnimationPath>
 #include <osg/Geometry>
 #include <osg/Group>
 #include <osg/MatrixTransform>
-#include <osg/Node>
 #include <osg/NodeCallback>
 #include <osg/PositionAttitudeTransform>
 #include <osg/TexEnv>
 #include <osg/TexGen>
 #include <osg/Texture2D>
-#include <osgDB/ReadFile>
-#include <osgAnimation/UpdateMatrixTransform>
 #include <osgAnimation/Animation>
+#include <osgAnimation/BasicAnimationManager>
 #include <osgAnimation/StackedScaleElement>
 #include <osgAnimation/StackedTranslateElement>
-#include <osgAnimation/BasicAnimationManager>
+#include <osgAnimation/UpdateMatrixTransform>
+#include <osgDB/ReadFile>
+#include <osgText/Font>
+#include <osgText/Text>
 
-class UAV: public osg::Node
+class UAV : public osg::Node
 {
 public:
     UAV();
@@ -27,14 +27,12 @@ public:
     osg::ref_ptr<osg::Node> CreateGlider(int a);
 
 protected:
-//    osg::ref_ptr<osg::PositionAttitudeTransform>
-//            GliderTrans;
+    //    osg::ref_ptr<osg::PositionAttitudeTransform>
+    //            GliderTrans;
     osg::ref_ptr<osg::PositionAttitudeTransform> LineTrans;
     osg::ref_ptr<osg::AnimationPath> GLiderPath;
     osg::ref_ptr<osg::AnimationPath> LinePath;
     osg::ref_ptr<osg::Group> LineGroup;
-
-
 };
 
-#endif // UAV_H
+#endif  // UAV_H
