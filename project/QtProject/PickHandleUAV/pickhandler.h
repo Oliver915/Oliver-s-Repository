6794971112ed263842af59b9c154ad6b7f78a7ff
Mengyu <785144001@qq.com>
2017-07-05 14:12:03 +0800

    add PickHandlerUAV

---
+++ project/QtProject/PickHandleUAV/pickhandler.h
@@ -0,0 +1,33 @@
+//#ifndef PICKHANDLER_H
+//#define PICKHANDLER_H
+//#include <iostream>
+//#include <osg/Geode>
+//#include <osg/Group>
+//#include <osg/MatrixTransform>
+//#include <osg/Node>
+//#include <osg/PolygonMode>
+//#include <osg/ShapeDrawable>
+
+//#include <osgGA/GUIEventHandler>
+//#include <osgViewer/Viewer>
+//#include <osgViewer/ViewerEventHandlers>
+
+//class PickHandler : public osgGA::GUIEventHandler
+//{
+//public:
+//    PickHandler()
+//        : _mX(0.f)
+//        , _mY(0.f)
+//    {
+//    }
+//    ~PickHandler() {}
+//    virtual bool handle(
+//            const osgGA::GUIEventAdapter& ea,
+//            osgGA::GUIActionAdapter& aa);
+
+//protected:
+//    float _mX, _mY;
+//    void pick(osg::ref_ptr<osgViewer::Viewer> viewer, float x, float y);
+//};
+
+//#endif  // PICKHANDLER_H
